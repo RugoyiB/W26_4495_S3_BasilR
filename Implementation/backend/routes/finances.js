01b9af2896210975e1ac8267f05e90a0dabed0b2
Basil Rugoyi <brugoyi@gmail.com>
2026-03-21 19:30:32 -0700

    Updated code to allow non church members to make donations.

--- Implementation/backend/routes/finances.js
+++ Implementation/backend/routes/finances.js
@@ -3,34 +3,32 @@ const router = express.Router();
 
 const auth = require("../middleware/auth");
 const authorize = require("../middleware/authorize");
-const auditLog = require("../middleware/auditMiddleware");
+const financeController = require("../controllers/financeController");
 
-const {
-  createFinance,
-  getAllFinance,
-  getFinanceReport
-} = require("../controllers/financeController");
+// Create finance record
+router.post("/", auth,
+  authorize("ADMIN", "TREASURER"),
+  // auditLog("Create Finance Record", "Finance"),
+  financeController.createFinance);
 
-router.post("/", auth, createFinance);
-router.get("/", auth, getAllFinance);
-router.get("/report", getFinanceReport);
+// Get all finance records
+router.get(
+  "/",
+  auth,
+  // authorize("ADMIN", "TREASURER"),
+  financeController.getAllFinance
+);
 
-router.get("/report",
+// Finance report
+router.get(
+  "/report",
   auth,
   authorize("ADMIN", "TREASURER"),
-  getFinanceReport
+  financeController.getFinanceReport
 );
 
-router.post(
-  "/",
-  auditLog("Create Finance Record", "Finance"),
-  financeController.createFinance
-);
-
-router.delete(
-  "/:id",
-  auditLog("Delete Finance Record", "Finance"),
-  financeController.deleteFinance
-);
+// Delete finance record
+router.delete("/:id", auth, authorize("ADMIN", "TREASURER"), financeController.deleteFinance);
+// auditLog("Delete Finance Record", "Finance"),
 
 module.exports = router;
