bc30db6064dceccb98551d2c4e88f005fed655e1
Basil Rugoyi <brugoyi@gmail.com>
2026-03-30 13:47:10 -0700

    Fixed minor validation issues.

--- Implementation/backend/models/Finance.js
+++ Implementation/backend/models/Finance.js
@@ -4,9 +4,9 @@ const financeSchema = new mongoose.Schema({
   member: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Member",
-    required: false // Allow null for non-members or guests
+    required: false //Allow null for non-members or guests
   },
-  donorName: { type: String }, // For non-members or guests
+  donorName: { type: String }, //For non-members or guests
   amount: {
     type: Number,
     required: true
