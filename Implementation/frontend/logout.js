0f46777b67e5d3ec3d3197117e5f1964de2002f9
Basil Rugoyi <brugoyi@gmail.com>
2026-04-02 19:14:10 -0700

    Added validation to the file.

--- Implementation/frontend/logout.js
+++ Implementation/frontend/logout.js
@@ -1,6 +1,20 @@
-function logout() {
+async function logout() {
+  const token = localStorage.getItem("token");
+
+  try {
+    await fetch("http://localhost:5000/api/auth/logout", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    });
+  } catch (err) {
+    console.error("Logout API failed");
+  }
+
   localStorage.clear();
   localStorage.removeItem("token");
   localStorage.removeItem("role");
   window.location.href = "welcome.html";
 }
+
